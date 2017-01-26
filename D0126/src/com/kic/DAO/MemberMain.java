d18ad2fc567105d8f54a574f1449152103dd0d0f
kitcoop <kitcoop@kitcoop-PC>
2017-01-26 17:45:01 +0900

    a2

---
+++ D0126/src/com/kic/DAO/MemberMain.java
@@ -0,0 +1,12 @@
+package com.kic.DAO;
+
+import com.kic.View.MemberView;
+
+public class MemberMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+new MemberView();
+	}
+
+}
---
+++ D0126/src/com/kic/View/MemberView.java
@@ -0,0 +1,49 @@
+package com.kic.View;
+
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+
+import com.kic.DAO.MemberDAO;
+
+public class MemberView extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	public MemberView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		MemberDAO dao=new MemberDAO();
+		Vector data=dao.showAll();
+		System.out.println(data.size());
+		Vector<String> title=new Vector<>();
+
+		title.add("id");
+		title.add("name");
+		title.add("age");
+		title.add("ki");
+		
+		
+		JTable table=new JTable(data, title);
+		JScrollPane pan=new JScrollPane(table);
+		pan.setBounds(20,20,500,100);
+		contentPane.add(pan);
+		
+		setBounds(10, 10, 600, 400);
+		setVisible(true);
+		
+		
+		
+	}
+}
